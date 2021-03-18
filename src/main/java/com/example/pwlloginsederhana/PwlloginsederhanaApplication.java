4ab0e38ce9a29b3e2d6d7ce6c085d48e162214fe
Riadi123 <44835552+Riadi123@users.noreply.github.com>
2021-03-19 06:00:09 +0700

    Tugas-Praktikum-PWL-1-

---
+++ src/main/java/com/example/pwlloginsederhana/PwlloginsederhanaApplication.java
@@ -0,0 +1,13 @@
+package com.example.pwlloginsederhana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PwlloginsederhanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PwlloginsederhanaApplication.class, args);
+	}
+
+}
